6074e601f374d262d8f6d24496dadf211b13c813
orianadienta <kadarsih122@gmail.com>
2025-12-21 02:29:55 +0800

    update firebase config

--- config/firebase.php
+++ config/firebase.php
@@ -1,6 +1,17 @@
 <?php
 
 return [
-    'credentials' => env('FIREBASE_CREDENTIALS', 
-        storage_path('app/firebase_credentials.json')),
+
+    'credentials' => env('FIREBASE_PRIVATE_KEY')
+        ? [
+            'type' => 'service_account',
+            'project_id' => env('FIREBASE_PROJECT_ID'),
+            'client_email' => env('FIREBASE_CLIENT_EMAIL'),
+            'private_key' => str_replace("\\n", "\n", env('FIREBASE_PRIVATE_KEY')),
+        ]
+        : env(
+            'FIREBASE_CREDENTIALS',
+            storage_path('app/firebase_credentials.json')
+        ),
+
 ];
