880f87b8cd55a318f106d6703e669e8642eae5de
orianadienta <kadarsih122@gmail.com>
2025-12-23 10:03:27 +0800

    update reminder

--- routes/console.php
+++ routes/console.php
@@ -23,7 +23,7 @@ Artisan::command('inspire', function () {
  * 7. Send via FCM ke devices
  */
 Schedule::job(new SendPeriodReminderJob())
-    ->dailyAt('10:00')
+    ->dailyAt('10:05')
     ->timezone('Asia/Makassar')
     ->name('dispatch-period-reminder-job')
     ->onOneServer()
