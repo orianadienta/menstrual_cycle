a21371e7ad5c9737ff392e9e9429c8c0ff6c3156
orianadienta <kadarsih122@gmail.com>
2025-12-23 12:06:13 +0800

    Fix railway worker start command

--- railway/run-worker.sh
+++ railway/run-worker.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 set -e
 
-php artisan queue:work --sleep=3 --tries=3 --timeout=90
+echo "==> Worker starting"
+php -v
+php artisan --version
+
+echo "==> Running queue worker"
+exec php artisan queue:work database --queue=default --sleep=3 --tries=3 --timeout=90 --verbose
